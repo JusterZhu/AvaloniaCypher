6d4f5f4828d6e1ae992aed26f1d68631f91e43da
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-08-28 17:51:47 +0800

    creat part14.

---
+++ Part14_ControlTheme/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Part14_ControlTheme.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
