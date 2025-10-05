466b68d829f35736e7249751bf9e8f91b5e4cf39
Juster Zhu <zhuzhen723723@163.com>
2025-10-05 23:31:46 +0800

    异步绑定

--- Demo/Part14_绑定/AvaloniaApplication2/AvaloniaApplication2/Bag.cs
+++ Demo/Part14_绑定/AvaloniaApplication2/AvaloniaApplication2/Bag.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -29,11 +32,29 @@ public class Bag : ObservableObject
 {
     //public ObservableCollection<string> Books { get; set; } = new ObservableCollection<string>();
 
+    public Task<string> MyText => GetTextAsync();
+
+    public async Task<string> GetTextAsync()
+    {
+        await Task.Delay(3000);
+        return "加载完成！显示最终结果！";
+    }
+
+    private Task<Bitmap> _myBitmap;
+    
+    public Task<Bitmap> MyBitmap
+    {
+        get => _myBitmap;
+        set => SetProperty(ref _myBitmap, value);
+    }
+    
     public double Price { get; set; } = 12.12;
     
     public DateTime CurrentDate { get; set; } = DateTime.Now;
     
     public RelayCommand<object> GetBookCommand { get; set; }
+    
+    public AsyncRelayCommand ShowImageCommand { get; set; }
     
     public ObservableCollection<Book> Books { get; set; } = new ObservableCollection<Book>();
 
@@ -60,9 +81,16 @@ public class Bag : ObservableObject
         });
         Books.Add(new Book("数学书", "李四"));
         Books.Add(new Book("英语书", "李雷"));
-
         GetBookCommand = new RelayCommand<object>(GetBook);
+        ShowImageCommand = new AsyncRelayCommand(ShowImageAction);
+        //MyBitmap = new Bitmap(new MemoryStream(File.ReadAllBytes(@"C:\51a9f3ce-66e4-4525-867c-7e8f12a70d6f.png")));
     }
+
+    private Task<Bitmap> ShowImageAction()
+    {
+        return MyBitmap = ImageHelper.LoadFromWeb(new Uri(@"https://th.bing.com/th/id/R.987f582c510be58755c4933cda68d525?rik=C0D21hJDYvXosw&riu=http%3a%2f%2fimg.pconline.com.cn%2fimages%2fupload%2fupc%2ftx%2fwallpaper%2f1305%2f16%2fc4%2f20990657_1368686545122.jpg&ehk=netN2qzcCVS4ALUQfDOwxAwFcy41oxC%2b0xTFvOYy5ds%3d&risl=&pid=ImgRaw&r=0"));
+    }
+
 
     private void GetBook(object? obj)
     {
